95f7d0331a913e4c2748fb8becf4ec9541ad92b7
Anik8127 <aniketsngh81@gmail.com>
2025-05-12 01:12:18 +0530

    changes

---
+++ Number_system.py
@@ -0,0 +1,6 @@
+print(0o27) # octal number
+print(0x1A) # hexadecimal number
+print(0b101010) # binary number
+print(27) # decimal number
+print(0o27 + 0x1A) # octal + hexadecimal
+print(0o27 + 0b101010) # octal + binary
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-#Eg. Call by Value
-def myfunc(lst):
-    lst.append(10)
-    print("Inside function:", lst)
-
-lst = [1, 2, 3]
-myfunc(lst)
-print("Outside function:", lst) #Original value of lst is changed
+print(0o27 + 0x1A) # octal + hexadecimal
+print(0o27 + 0b101010) # octal + binary
